945ed89a6419e80cde5ae7e0e7c80941678b7819
LoganDark <logandarklock@gmail.com>
2022-05-05 06:15:29 -0700

    fix typo in argon2.type_to_string

--- argon2.lua
+++ argon2.lua
@@ -192,7 +192,7 @@ function module.error_message(code)
 end
 
 function module.type_to_string(type, uppercase)
-	return ffi_string(argon2_type2string(type,·uppercase·and·1 or 0))
+	return ffi_string(argon2_type2string(type, uppercase and 1 or 0))
 end
 
 function module.encoded_len(
